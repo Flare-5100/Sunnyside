ae994c7fa26a97e64bb672a3128452a217fefde5
Vivek <vivekgajula2009@gmail.com>
2021-08-25 19:18:12 +0530

    Added Responsiveness

---
+++ script.js
@@ -0,0 +1,11 @@
+const menu = document.getElementById("menu");
+const nav_items = document.getElementsByClassName("nav-items")[0];
+const hero_title = document.getElementsByClassName("hero-title")[0];
+const down_arrow = document.getElementsByClassName("down-arrow")[0];
+
+menu.addEventListener("click", (e) => {
+    menu.classList.toggle("active");
+    nav_items.classList.toggle("visible");
+    hero_title.classList.toggle("visible");
+    down_arrow.classList.toggle("visible");
+});
